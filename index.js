2514efb5c73b18f7b12cb6024481b8b0aaea9bb8
Philipp Wallrich <p.wallrich@gmail.com>
2018-09-02 01:28:25 +0200

    Added first server

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 var Gpio = require('pigpio').Gpio;
+var http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('Hello Node.js Server!');
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log('server is listening on ${port}');
+})
 
 console.log("Define each color from RGB Strip");
 var ledRed = new Gpio(17, {mode: Gpio.output});
