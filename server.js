a75788e4e9ec6cf580a5ef85eab228f71a7996cc
Philipp Wallrich <p.wallrich@gmail.com>
2018-09-02 02:09:25 +0200

    Added not found handling

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/rootRoute');
 routes(app);
 
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
 app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
