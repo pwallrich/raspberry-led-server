52ebfe541744d238d8c8f7dbbc8ee46f4b269d87
Philipp Wallrich <p.wallrich@gmail.com>
2018-09-02 03:05:43 +0200

    Advanced logging

--- server.js
+++ server.js
@@ -32,6 +32,12 @@ function handler(req, res) {
 
 
 io.sockets.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  })
+  
   socket.on('rgbLed', function(data) {
     console.log(data);
 
